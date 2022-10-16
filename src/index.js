e3b94920fd94223b51902dd6fc4e79d8109c8244
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-16 02:33:00 -0700

    Updated and added some UI functions to display answers in the Dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+let ageCalculator = (event) => {
+  event.preventDefault();
+
+  let planet = document.getElementById("optionPlanet").value;
+  let age = document.getElementById("age").value;
+
+  document.getElementById("planetOpt").innerHTML = planet;
+  document.getElementById("yearsOld").innerHTML = age;
+
+};
+
 window.addEventListener("load", ()=> {
+  document.getElementById("aged").addEventListener("submit", ageCalculator);
 
 });
